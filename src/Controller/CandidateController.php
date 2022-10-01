7857320a0227bf2b0159e0b67d2d8bbb8b5dba1b
Rania Yqb <raniayaqoubi1995@gmail.com>
2022-10-01 14:21:07 +0200

    adding data and Symfony Serializer

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/candidate', name: 'app_candidate')]
+#[Route('/candidat', name: 'app_candidate')]
 class CandidateController extends AbstractController
 {
     #[Route('/', name: '')]
