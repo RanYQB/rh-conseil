b040635963e0c7f2f310ad8723e590390e13b20b
Rania Yqb <raniayaqoubi1995@gmail.com>
2022-10-02 15:10:23 +0200

    FileUploader service

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -3,13 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Candidate;
+use App\Entity\Resume;
 use App\Form\CandidateType;
+use App\Form\ResumeType;
 use App\Repository\CandidateRepository;
+use App\Services\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/candidat', name: 'app_candidate')]
 class CandidateController extends AbstractController
@@ -36,7 +42,7 @@ class CandidateController extends AbstractController
 
 
     #[Route('/completer-profil', name: '_complete_profile')]
-    public function completeProfile(Request $request , EntityManagerInterface $entityManager, CandidateRepository $candidateRepository): Response
+    public function completeProfile(Request $request , FileUploader $fileUploader, EntityManagerInterface $entityManager, CandidateRepository $candidateRepository): Response
     {
 
         if ($this->isGranted('ROLE_CANDIDATE')) {
@@ -50,14 +56,33 @@ class CandidateController extends AbstractController
 
 
             $candidate = new Candidate();
-            $form = $this->createForm(CandidateType::class, $candidate);
+            $resume = new Resume();
+            $items = ['candidate' => $candidate, 'resume' => $resume];
+            $form = $this->createFormBuilder($items)
+                ->add('candidate', CandidateType::class)
+                ->add('resume', ResumeType::class)
+                ->getForm();
             $form->handleRequest($request);
 
             if($form->isSubmitted() && $form->isValid()) {
 
+                /** @var UploadedFile $resumeFileFile */
+                $resumeFile = $form->get('resume')["name"]->getData();
+
+                if ($resumeFile) {
+                    $resumeFileName = $fileUploader->upload($resumeFile);
+                    $resume->setName($resumeFileName);
+                    $resume->setPath('uploads/resumes/' . $resumeFileName);
+
+                }
+
                 $candidate->setUser($user);
+                $candidate->setResume($resume);
                 $entityManager->persist($candidate);
+                $resume->setCandidate($candidate);
+                $entityManager->persist($resume);
                 $entityManager->flush();
+
                 $this->addFlash('success', 'Votre profil a bien été complété.');
                 return $this->redirectToRoute('app_candidate');
 
---
+++ src/Form/ResumeType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Resume;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+
+class ResumeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', FileType::class, [
+                'label' => 'Cv (au format PDF)',
+                'mapped' => false,
+                'required' => true,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/x-pdf',
+                        ],
+                        'mimeTypesMessage' => 'Merci de télécharger un fichier au format PDF',
+                    ])
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Resume::class,
+        ]);
+    }
+}
